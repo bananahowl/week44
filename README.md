8e84a6f4340e4dc5571c26eb5c5ce9ef51183ec4
A O <ahmedothman8@hotmail.com>
2019-11-01 20:41:26 +0100

    chekcpush

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 ## Week 44 assignment 
 
-In the project directory, there is js files for one or several react assignments for the week. 
+In the project directory, there is .js files for one or several react assignments for the week. 
 they are set down below.
 <br>
-It should be noted that there have been not made a lot of work because of sickness which have lasted til friday. 
+It should be noted that there have been not made a lot of work
+because of a pretty bad sickness which have lasted til friday. 
 
 
 ### folder with all the react code 
  [folder](https://github.com/bananahowl/week44/tree/master/src)
 ### day1
-  - files: [link]( )
- - 
+  - day1: are links ex1,ex2,ex3
+  -  day2(use states and use effect): ex4,ex5
+  - 
--- src/AppDay2p3.js
+++ src/AppDay2p3.js
@@ -4,8 +4,10 @@ import {useState}from 'react';
 
 export default function AppDay2p3() {
 
-  const [chuck, setChuck] = useState();
+  const [chuck, setChuck] = useState({ value: ""});
   const [selector, setSelector] = useState(false);
+
+  const [dad, setDad] = useState({value: ""});
 
   useEffect( () => {
 
@@ -13,23 +15,43 @@ export default function AppDay2p3() {
 
         fetch('https://api.chucknorris.io/jokes/random')
         .then(res => res.json())
-        .then(chucks => {setChuck(chucks)})
+        .then(data => {setChuck(data)})
+        .then(data => {console.log(data)})
     }
     getChuk()
   },[selector])
+  // tried to do as seen on a website to make a timer to fetch the data
+  useEffect(()=> {
+      const timer = setTimeout(()=>{
+         function getDad(){
+            fetch('https://icanhazdadjoke.com/')
+            .then(res => res.json())
+            .then(data => {setDad(data)})  
+         } 
+         getDad()
+      },10000)
+
+  },[])
 
   /* var date = Date();
   var curenTime =  date.toLocaleTimeString(); */
 
 //onChange={val => setcount(val.target.value)}
 
+/* 
+
 // https://api.chucknorris.io/jokes/random 
+https://icanhazdadjoke.com/api
+
+ */
   return (
     <div className="App">
       <h2>day 2 ex 3 </h2>
 
-      <button onClick={()=>setSelector(!selector)}> press to get a joke</button>
+      <button onClick={()=>setSelector(!selector)}> press to Get ChuckNorris</button>
       <p> chuck joke : {chuck.value}</p>
+
+      <p> dad joke : {dad.value}</p>
       
       
     </div>
